80efd7feb31d2b98dd2e07ce50a576e3c986dc84
Sangjung Woo <sangjung.woo@samsung.com>
2022-05-13 00:29:21 +0900

    Add basic example
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

---
+++ basic_v1/client.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+#include "service-test.h"
+
+int main (int argc, char* argv[])
+{
+  MachinelearningServiceCalc *proxy;
+  GError *error = NULL;
+  gint result;
+  int lvalue, rvalue;
+  lvalue = atoi (argv[1]);
+  rvalue = atoi (argv[2]);
+
+
+  fprintf(stderr, "Client started!\n");
+  proxy = machinelearning_service_calc_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
+                                                      G_DBUS_PROXY_FLAGS_NONE,
+                                                      "org.tizen.machinelearning.service",
+                                                      "/Org/Tizen/MachineLearning/Service",
+                                                      NULL, &error);
+  if (error != NULL) {
+    fprintf(stderr, "Failed to machinelearning_service_calc_proxy_new_sync(): %s\n", error->message);
+    g_error_free (error);
+    return -1;
+  }
+
+  machinelearning_service_calc_call_add_sync (proxy, lvalue, rvalue, &result, NULL, &error);
+  if (error != NULL) {
+    fprintf(stderr, "Failed to machinelearning_service_calc_call_add_sync(): %s\n", error->message);
+    g_error_free (error);
+    return -1;
+  }
+
+  fprintf(stderr, "result: %d\n", result);
+
+  g_object_unref (proxy);
+  
+  return 0;
+}
---
+++ basic_v1/service.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+#include "service-test.h"
+
+guint owner_id;
+GMainLoop *loop;
+
+static gboolean
+handler_add (MachinelearningServiceCalc *interface,
+            GDBusMethodInvocation *invocation,
+            int left, int right, gpointer user_data)
+{
+  int result = left + right;
+  fprintf(stderr, "handler_add(): %d + %d = %d\n", left, right, result);
+
+  machinelearning_service_calc_complete_add (interface, invocation, result);
+
+  return TRUE;
+}
+
+
+static void
+on_bus_acquired (GDBusConnection *connection,
+                const gchar *name,
+                gpointer user_data)
+{
+  MachinelearningServiceCalc *interface;
+  gboolean ret;
+  GError *error = NULL;
+
+  fprintf(stderr, "Acquire name: %s\n", name);
+
+  interface = machinelearning_service_calc_skeleton_new ();
+
+  g_signal_connect (interface, "handle-add",
+                  G_CALLBACK (handler_add), NULL);
+
+  ret = g_dbus_interface_skeleton_export(
+    G_DBUS_INTERFACE_SKELETON(interface),
+    connection,
+    "/Org/Tizen/MachineLearning/Service", &error);
+  if (!ret) {
+    fprintf (stderr, "Failed to export: \n");
+    g_free (error);
+  }
+}
+
+
+int main (int argc, char* argv[])
+{
+  loop = g_main_loop_new (NULL, FALSE);
+
+  owner_id = g_bus_own_name (G_BUS_TYPE_SESSION, 
+                          "org.tizen.machinelearning.service",
+                          G_BUS_NAME_OWNER_FLAGS_NONE, NULL,
+                          on_bus_acquired, NULL, NULL, NULL);
+  if (owner_id == 0) {
+    fprintf(stderr, "Failed to get gdbus own name\n");
+    exit (-1);
+  }
+
+  g_main_loop_run (loop);
+
+  g_main_loop_unref (loop);
+
+  return 0;
+}
+
+
+
+
+
