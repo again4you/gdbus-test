823a09505d8a0f1991b332232a769bfa719de55c
Sangjung Woo <sangjung.woo@samsung.com>
2022-06-16 00:09:26 +0900

    wip: test
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

--- basic_v2/service.c
+++ basic_v2/service.c
@@ -4,17 +4,116 @@
 
 #include "service-test.h"
 
+/****************************************************************************************/
+
 #define DBUS_MACHINELEARNING_SERVICE_INTERFACE  "org.tizen.machinelearning.service"
+#define DBUS_MACHINELEARNING_SERVICE_PATH  "/Org/Tizen/MachineLearning/Service"
+
+#define DBUS_MACHINELEARNING_HANDLE_ADD   "handle-add"
+
+#define ARRAY_SIZE(name) (sizeof(name)/sizeof(name[0]))
+
+/****************************************************************************************/
+struct gdbus_signal_info {
+  const gchar *handler;
+  GCallback cb;
+  gpointer cb_data;
+  gulong ret_id;
+};
+
+/****************************************************************************************/
 
 GMainLoop *loop;
 static GDBusConnection *g_dbus_sys_conn[2] = {NULL, };
 static MachinelearningService *g_gdbus_instance = NULL;
 
+/****************************************************************************************/
+
+static gboolean
+handler_add (MachinelearningService *interface,
+            GDBusMethodInvocation *invocation,
+            int left, int right, gpointer user_data)
+{
+  int result = left + right;
+  fprintf(stderr, "handler_add(): %d + %d = %d\n", left, right, result);
+
+  machinelearning_service_complete_add (interface, invocation, result);
+
+  return TRUE;
+}
+
+static struct gdbus_signal_info gdbus_signal_service_infos[] = {
+  {
+    .handler = DBUS_MACHINELEARNING_HANDLE_ADD,
+    .cb = G_CALLBACK(handler_add),
+    .cb_data = NULL,
+    .ret_id = 0,
+  },
+};
+
+
+/************************************************************************/
 
 MachinelearningService *gdbus_get_instance_service (void)
 {
   return machinelearning_service_skeleton_new ();
 }
+
+int gdbus_get_system_connection (int idx)
+{
+  GError *error = NULL;
+  g_dbus_sys_conn[idx] = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, &error);
+  // g_dbus_sys_conn[idx] = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, &error);
+
+  if (g_dbus_sys_conn[idx] == NULL) {
+    fprintf(stderr, "Failed to get SYSTEM_BUS\n");
+    return -1;
+  }
+
+  fprintf(stderr, "Success to get SYSTEM_BUS\n");
+  return 0;
+}
+
+int gdbus_connect_signal (gpointer instance, int num_signal,
+  struct gdbus_signal_info *signal_infos)
+{
+  int i;
+  unsigned long handler_id;
+
+  for (i = 0; i < num_signal; ++i) {
+    handler_id = g_signal_connect (instance,
+      signal_infos[i].handler,
+      signal_infos[i].cb,
+      signal_infos[i].cb_data);
+
+    signal_infos[i].ret_id = handler_id;
+  }
+  return 0;
+}
+
+int gdbus_export_interface (int idx, gpointer instance,
+  const char * obj_path)
+{
+  gboolean ret;
+
+  if (g_dbus_sys_conn[idx] == NULL) {
+    fprintf(stderr, "Failed to get SYSTEM_BUS\n");
+    return -1;
+  }
+
+  ret = g_dbus_interface_skeleton_export (
+    G_DBUS_INTERFACE_SKELETON(instance),
+    g_dbus_sys_conn[idx],
+    obj_path, NULL);
+
+  if (!ret) {
+    fprintf(stderr, "Failed to g_dbus_interface_skeleton_export()\n");
+    return -1;
+  }
+  return 0;
+}
+
+/************************************************************************/
 
 int service_module_probe ()
 {
@@ -27,6 +126,21 @@ int service_module_probe ()
     return -1;
   }
 
+  ret = gdbus_connect_signal (g_gdbus_instance,
+    ARRAY_SIZE(gdbus_signal_service_infos), gdbus_signal_service_infos);
+  if (ret < 0) {
+    fprintf(stderr, "Failed to register callbacks for dbus method\n");
+    return -1;
+  }
+
+  ret = gdbus_export_interface (0,
+    g_gdbus_instance, DBUS_MACHINELEARNING_SERVICE_PATH);
+  if (ret < 0) {
+    fprintf(stderr, "Failed to register callbacks for dbus method\n");
+    return -1;
+  }
+
+  fprintf(stderr, "Success to service_module_probe()\n");
   return 0;
 }
 
@@ -36,19 +150,7 @@ void init_modules ()
   service_module_probe();
 }
 
-int gdbus_get_system_connection (int idx)
-{
-  GError *error = NULL;
-  g_dbus_sys_conn[idx] = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, &error);
 
-  if (g_dbus_sys_conn[idx] == NULL) {
-    fprintf(stderr, "Failed to get SYSTEM_BUS\n");
-    return -1;
-  }
-
-  fprintf(stderr, "Success to get SYSTEM_BUS\n");
-  return 0;
-}
 
 #if 0
 guint owner_id;
@@ -101,6 +203,10 @@ int main (int argc, char* argv[])
   loop = g_main_loop_new (NULL, FALSE);
 
   gdbus_get_system_connection (0);
+
+  init_modules();
+
+  g_main_loop_run (loop);
 
   g_main_loop_unref (loop);
 #if 0
